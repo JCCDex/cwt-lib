303c775d1ee903c194979eac023716d0cf662531
maincc <1316788824@qq.com>
2024-06-13 10:37:54 +0800

    Modify test file

--- lib/src/chainToken.d.ts
+++ lib/src/chainToken.d.ts
@@ -4,11 +4,11 @@ export default class ChainToken {
     readonly chain: string;
     readonly alg: string;
     wallet: any;
-    constructor(chain: string, alg: string, key: string, keyType: string);
+    constructor(chain: string, key: string, keyType: string);
     sign(data: {
         header: any;
         payload: any;
     }, format?: string): string;
-    static quickSign(key: string, usr: string, chain: string, alg: string): string;
+    static quickSign(key: string, usr: string, chain: string): string;
     verify(token: string, format?: string): boolean;
 }
