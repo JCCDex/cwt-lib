ca4c309386876b2473941bb0a2cd626b266118fe
GinMu <gin.musheng@gmail.com>
2024-06-27 20:51:31 +0800

    rename KeyEncoder.ts

--- src/keypairs/ed25519-keypair.ts
+++ src/keypairs/ed25519-keypair.ts
@@ -1,13 +1,17 @@
 import KeyPair from "./keypair";
 import { escape, encode, unescape } from "jsontokens/lib/base64Url.js";
 import { IKeyPair } from "../type";
-import KeyEncoder from "../KeyEncoder";
-import { eddsa as EdDSA } from 'elliptic'
+import KeyEncoder from "../key-encoder";
+import { eddsa as EdDSA } from "elliptic";
 
 export default class Ed25519KeyPair extends KeyPair {
-  ED: EdDSA = EdDSA('ed25519');
+  private ED: EdDSA;
+  private keyEncoder: KeyEncoder;
+
   constructor(keypair: IKeyPair) {
     super(keypair);
+    this.ED = EdDSA("ed25519");
+    this.keyEncoder = new KeyEncoder("ed25519");
   }
 
   public sign(data: Record<string, unknown>): string {
@@ -19,18 +23,20 @@ export default class Ed25519KeyPair extends KeyPair {
   }
 
   public getPublicPem(): string | Buffer {
-    const keyEncoder = new KeyEncoder('ed25519');
-    return keyEncoder.encodePublic(this.publicKey)+"\n";
+    return this.keyEncoder.encodePublic(this.publicKey) + "\n";
   }
 
   public getPrivatePem(): string | Buffer {
-    const keyEncoder = new KeyEncoder('ed25519');
-    return keyEncoder.encodePrivate(this.privateKey)+"\n";
+    return this.keyEncoder.encodePrivate(this.privateKey) + "\n";
   }
 
   public verify(token: string): boolean {
     const [header, payload, signature] = token.split(".");
     const signData = header + "." + payload;
-    return this.ED.verify(Buffer.from(signData), Buffer.from(unescape(signature), 'base64').toString('hex'), this.publicKey);
+    return this.ED.verify(
+      Buffer.from(signData),
+      Buffer.from(unescape(signature), "base64").toString("hex"),
+      this.publicKey
+    );
   }
 }
